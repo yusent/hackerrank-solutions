d79bbd38c01c6e163f1f8cf8d26957ff90296f84
Yusent Chig <yusent@protonmail.com>
2020-08-19 12:42:03 -0700

    Improve password-cracker-fp solution to pass all tests

--- password-cracker-fp.hs
+++ password-cracker-fp.hs
@@ -1,21 +1,32 @@
 -- Challenge url: https://www.hackerrank.com/challenges/password-cracker-fp/problem
 
 import Control.Monad (replicateM_)
+import Data.Function (on)
+import Data.List (find, sortBy)
+import Data.Maybe (fromJust, isJust)
 
 main :: IO ()
 main = do
   t <- readLn
   replicateM_ t $ do
     _ <- getLine
-    passwords <- words <$> getLine
+    passwords <- removeRedundantPwds . words <$> getLine
     loginAttempt <- getLine
     putStrLn $ maybe "WRONG PASSWORD" unwords $ validate passwords loginAttempt
 
+removeRedundantPwds :: [String] -> [String]
+removeRedundantPwds = removeRedundantPwds' . reverse . sortBy (compare `on` length)
+  where
+    removeRedundantPwds' [] = []
+    removeRedundantPwds' (password : passwords)
+      | isJust $ validate passwords password = removeRedundantPwds' passwords
+      | otherwise = password : removeRedundantPwds' passwords
+
 validate :: [String] -> String -> Maybe [String]
 validate _ [] = Just []
-validate passwords string = firstJust $ splitPrefix <$> passwords
+validate passwords string = fmap fromJust . find isJust $ split <$> passwords
   where
-    splitPrefix pass = fmap (pass :) . validate passwords =<< removeIfPrefix pass string
+    split pass = fmap (pass :) . validate passwords =<< removeIfPrefix pass string
 
 removeIfPrefix :: String -> String -> Maybe String
 removeIfPrefix [] result = Just result
@@ -23,8 +34,3 @@ removeIfPrefix _ [] = Nothing
 removeIfPrefix (x : xs) (y : ys)
   | x == y = removeIfPrefix xs ys
   | otherwise = Nothing
-
-firstJust :: [Maybe a] -> Maybe a
-firstJust [] = Nothing
-firstJust (Nothing : xs) = firstJust xs
-firstJust (Just x : _) = Just x
